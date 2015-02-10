8996506964b54af6706be1e099b0bf83326e99fe
Hackbright Student <info@hackbrightacademy.com>
2015-02-09 18:04:51 -0800

    First day of work on movie ratings app mega project, started adding functions to process datafiles

--- model.py
+++ model.py
@@ -1,16 +1,52 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable = True)
+    zipcode = Column(String(15), nullable = True)
 
-
+    def __repr__(self):
+        """Cleanly show info about self """
+        return "<User id=%d email=%s password=%s age=%d zipcode=%s>" % (self.id, self.email, self.password, self.age, self.zipcode)
 ### End class declarations
 
+class Movie(Base): 
+    __tablename__ = "movies"
+    id = Column(Integer, primary_key = True)
+    title = Column(String(64), nullable = False)
+    rel_date = Column(DateTime, nullable = False)
+    url = Column(String(150), nullable = False)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, nullable = False)
+    movie_id = Column(Integer, nullable = False)
+    rating = Column(Integer, nullable = False)
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
+
 def main():
-    """In case we need this for something"""
+    """In case we need   this for something"""
     pass
 
 if __name__ == "__main__":
--- seed.py
+++ seed.py
@@ -3,7 +3,13 @@ import csv
 
 def load_users(session):
     # use u.user
-    pass
+    with open('seed_data/u.user') as file:
+        reader = csv.reader(file, delimiter='|')    # reader function takes each line of the file & makes a list containing all that line's columns
+        for row in reader:
+            id, age, gender, occupation, zipcode = row  # unpacking row   
+            u = User(id=id,email=None, password=None, age=age, zipcode=zipcode)
+            session.add(u)
+    session.commit()
 
 def load_movies(session):
     # use u.item
