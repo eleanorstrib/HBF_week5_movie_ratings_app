4b6b7211d684d5346fc56e570088c8264e88ae94
Hackbright Student <info@hackbrightacademy.com>
2015-02-10 18:10:52 -0800

    data looks good and tables populating correctly

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, backref
 
 ENGINE = None
 Session = None
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 ### Class declarations go here
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "Users"
     id = Column(Integer, primary_key = True)
     email = Column(String(64), nullable = True)
     password = Column(String(64), nullable = True)
@@ -23,18 +23,29 @@ class User(Base):
 ### End class declarations
 
 class Movie(Base): 
-    __tablename__ = "movies"
+    __tablename__ = "Movies"
     id = Column(Integer, primary_key = True)
     title = Column(String(64), nullable = False)
-    rel_date = Column(DateTime, nullable = False)
+    rel_date = Column(DateTime, nullable = True)
     url = Column(String(150), nullable = False)
+    
+    def __repr__(self):
+        """Cleanly show info about self """
+        return "<id=%d title=%s rel_date=%s url=%s>" % (self.id, self.title, self.rel_date, self.url)
 
 class Rating(Base):
-    __tablename__ = "ratings"
+    __tablename__ = "Ratings"
     id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, nullable = False)
-    movie_id = Column(Integer, nullable = False)
-    rating = Column(Integer, nullable = False)
+    user_id = Column(Integer, ForeignKey('Users.id'))
+    movie_id = Column(Integer, ForeignKey('Movies.id'))
+    rating = Column(Integer)
+
+    user = relationship("User",
+            backref=backref("Ratings", order_by=id))
+
+    def __repr__(self):
+        """Cleanly show info about self """
+        return "<id=%d user_id=%d movie_id=%d rating=%d>" % (self.id, self.user_id, self.movie_id, self.rating)
 
 def connect():
     global ENGINE
@@ -46,7 +57,7 @@ def connect():
     return Session()
 
 def main():
-    """In case we need   this for something"""
+    # session = connect()
     pass
 
 if __name__ == "__main__":
